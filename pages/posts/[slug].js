1b8f43df1244be3e77cf4b4b53048366cc834a63
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-07-29 13:41:31 +1000

    Setting up the Core Pages

---
+++ pages/posts/[slug].js
@@ -0,0 +1,5 @@
+function PostDetailPage() {
+  return <h1>PostDetailPage</h1>;
+}
+
+export default PostDetailPage;
---
+++ pages/posts/index.js
@@ -0,0 +1,5 @@
+function AllPostsPage() {
+  return <h1>AllPostsPage</h1>;
+}
+
+export default AllPostsPage;
