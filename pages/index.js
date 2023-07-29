5340fe6cabbeee91a92ec29f102a29a8fb8734fe
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-07-29 14:44:18 +1000

    Rendering Dummy Post Data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
+import FeaturedPosts from '@/components/home-page/featured-posts';
 import Hero from '@/components/home-page/hero';
 
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Nextjs is a react framework for Production ',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Nextjs is a react framework for Production ',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Nextjs is a react framework for Production ',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Nextjs is a react framework for Production ',
+    date: '2022-02-10',
+  },
+];
 function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
