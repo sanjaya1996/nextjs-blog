515128a11c5d3ac0d30f16849dd50f02b7a75690
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-07-30 13:20:26 +1000

    Rendering images with the Next Image component in Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -7,10 +8,36 @@ function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customComponents = {
+    p(paragraph) {
+      const { node } = paragraph;
+      console.log(node);
+      if (node.children[0].tagName === 'img') {
+        const { alt, src } = node.children[0].properties;
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${src}`}
+              alt={alt}
+              height={300}
+              width={600}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 }
