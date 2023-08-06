278fa872e96dcb42c9416e3240b09c087c217cb0
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-08-06 17:54:44 +1000

    Adding head data

--- components/posts/all-posts.js
+++ components/posts/all-posts.js
@@ -1,12 +1,22 @@
+import Head from 'next/head';
 import classes from './all-posts.module.css';
 import PostsGrid from './posts-grid';
 
 function AllPosts(props) {
   return (
-    <section className={classes.posts}>
-      <h1>All Posts</h1>
-      <PostsGrid posts={props.posts} />
-    </section>
+    <>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name='description'
+          content='A list of all programming related tutorials and posts'
+        />
+      </Head>
+      <section className={classes.posts}>
+        <h1>All Posts</h1>
+        <PostsGrid posts={props.posts} />
+      </section>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import FeaturedPosts from '@/components/home-page/featured-posts';
 import Hero from '@/components/home-page/hero';
 import { getFeaturedPosts } from '@/lib/posts-util';
@@ -5,6 +6,13 @@ import { getFeaturedPosts } from '@/lib/posts-util';
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Max Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development'
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
